4e0f7267c7b55ae96aa9330044e66c37c2aeb1df
haoyh19 <1398024210@qq.com>
2021-09-03 11:03:32 +0800

    add two classes: Beacon and Mine

---
+++ EDCHost21/Beacon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDCHOST22
+{
+    class Beacon    // 信标
+    {
+        public const int TOTAL = 6; // 每场比赛最多设置6个信标
+
+        public int num;     // 已设置信标的个数
+        public Dot[] dots;  // 设置信标的位置信息，0≤小标≤num-1的数据有效
+        public void ResetBeacon()   // 重置信标个数为0
+        {
+            num = 0;
+        }
+        public Beacon(int Num = 0)      // 构造函数
+        {
+            num = Num;
+            dots = new Dot[TOTAL];
+            Dot temp = new Dot();
+            for (int i = 0; i < TOTAL; i++)
+            {
+                dots[i] = temp;
+            }
+        }
+    }
+}
---
+++ EDCHost21/Mine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDCHOST22
+{
+    class Mine      // 此类第一回合、第二回合通用，两个回合分别使用不同的generator生成金矿
+    {
+        public Dot Start_Dot;       // 金矿初始位置
+        public Dot End_Dot;         // 需要运输到的点（停车区域的中心点）
+
+        //构造函数
+        public Mine(Dot start_dt, Dot end_dt)
+        {
+            Start_Dot = start_dt;
+            End_Dot = end_dt;
+        }
+
+        //用于修改点位的接口
+        public void ResetDot(Dot start_dt, Dot end_dt)
+        {
+            Start_Dot = start_dt;
+            End_Dot = end_dt;
+        }
+
+    }
+}
