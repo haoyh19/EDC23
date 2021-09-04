aac783cb3094f288b6b56b522e70e7f5138b4479
shixiaoy20 <75681444+shixiaoy20@users.noreply.github.com>
2021-09-04 17:18:12 +0800

    Update Beacon.cs
    将两车设置的信标设置为两种信标，便于区分，同时增加了计算信标同小车距离的接口

--- EDCHost21/Beacon.cs
+++ EDCHost21/Beacon.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,25 +6,110 @@ using System.Threading.Tasks;
 
 namespace EDCHOST22
 {
-    class Beacon    // 信标
+    public class Beacon
     {
-        public const int TOTAL = 6; // 每场比赛最多设置6个信标
-
-        public int num;     // 已设置信标的个数
-        public Dot[] dots;  // 设置信标的位置信息，0≤小标≤num-1的数据有效
-        public void ResetBeacon()   // 重置信标个数为0
+        //CarA放置的信标
+        public Dot[] CarABeacon;
+        //CarB放置的信标
+        public Dot[] CarBBeacon;
+        //一辆车最大允许放置的信标
+        public int MaxBeaconNum;
+        //CarA放置的信标数量
+        public int CarABeaconNum;
+        //CarB放置的信标数量
+        public int CarBBeaconNum;
+        //构造函数
+        public Beacon()
         {
-            num = 0;
+            //一辆车放置的信标数量最大为3
+            MaxBeaconNum = 3;
+            CarABeacon = new Dot[MaxBeaconNum];
+            CarBBeacon = new Dot[MaxBeaconNum];
+            CarABeaconNum = 0;
+            CarBBeaconNum = 0;
         }
-        public Beacon(int Num = 0)      // 构造函数
+        //重设信标
+        public void Reset()
         {
-            num = Num;
-            dots = new Dot[TOTAL];
-            Dot temp = new Dot();
-            for (int i = 0; i < TOTAL; i++)
+            CarABeaconNum = 0;
+            CarBBeaconNum = 0;
+        }
+        //CarA放置信标
+        public void CarAAddBeacon(Dot Pos)
+        {
+            //放置的信标不多于MaxBeaconNum
+            if (CarABeaconNum < MaxBeaconNum)
             {
-                dots[i] = temp;
+                CarABeacon[CarABeaconNum] = Pos;
+                CarABeaconNum++;
             }
+        }
+        //CarB放置信标
+        public void CarBAddBeacon(Dot Pos)
+        {
+            if (CarBBeaconNum < 3)
+            {
+                CarBBeacon[CarBBeaconNum] = Pos;
+                CarBBeaconNum++;
+            }
+        }
+        //得到CarA同六个信标的距离，返回6个数字，前三个为自己放置的信标，后三个为另一组放置的信标，
+        //如果某一方未放置够3个信标，未放置的信标所返回的距离为-1
+        public double[] GetCarADistance(int Posx, int Posy)
+        {
+            double[] Distance = new double[MaxBeaconNum * 2];
+            for (int i = 0; i < MaxBeaconNum; i++)
+            {
+                if (i < CarABeaconNum)
+                {
+                    Distance[i] = Math.Sqrt((Posx - CarABeacon[i].x) * (Posx - CarABeacon[i].x) + (Posy - CarABeacon[i].y) * (Posy - CarABeacon[i].y));
+                }
+                else
+                {
+                    Distance[i] = -1;
+                }
+            }
+            for (int i = 0; i < MaxBeaconNum; i++)
+            {
+                if (i < CarBBeaconNum)
+                {
+                    Distance[i + MaxBeaconNum] = Math.Sqrt((Posx - CarBBeacon[i].x) * (Posx - CarBBeacon[i].x) + (Posy - CarBBeacon[i].y) * (Posy - CarBBeacon[i].y));
+                }
+                else
+                {
+                    Distance[i + MaxBeaconNum] = -1;
+                }
+            }
+            return Distance;
+        }
+        //得到CarB同六个信标的距离，返回6个数字，前三个为自己放置的信标，后三个为另一组放置的信标，
+        //如果某一方未放置够3个信标，未放置的信标所返回的距离为-1
+        public double[] GetCarBDistance(int Posx, int Posy)
+        {
+            double[] Distance = new double[MaxBeaconNum * 2];
+            for (int i = 0; i < MaxBeaconNum; i++)
+            {
+                if (i < CarBBeaconNum)
+                {
+                    Distance[i] = Math.Sqrt((Posx - CarBBeacon[i].x) * (Posx - CarBBeacon[i].x) + (Posy - CarBBeacon[i].y) * (Posy - CarBBeacon[i].y));
+                }
+                else
+                {
+                    Distance[i] = -1;
+                }
+            }
+            for (int i = 0; i < MaxBeaconNum; i++)
+            {
+                if (i < CarABeaconNum)
+                {
+                    Distance[i + MaxBeaconNum] = Math.Sqrt((Posx - CarABeacon[i].x) * (Posx - CarABeacon[i].x) + (Posy - CarABeacon[i].y) * (Posy - CarABeacon[i].y));
+                }
+                else
+                {
+                    Distance[i + MaxBeaconNum] = -1;
+                }
+            }
+            return Distance;
         }
     }
 }
