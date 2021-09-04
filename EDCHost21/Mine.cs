42c4e0875fa056f965fdd0a60b92ea60cb5722eb
haoyh19 <1398024210@qq.com>
2021-09-04 14:17:08 +0800

    修改Mine类代码

--- EDCHost21/Mine.cs
+++ EDCHost21/Mine.cs
@@ -6,42 +6,39 @@ using System.Threading.Tasks;
 
 namespace EDCHOST22
 {
-    class Mine      // 此类第一回合、第二回合通用，两个回合分别使用不同的generator生成金矿
+    class Mine      // 此类第一回合、第二回合通用，两个回合中用同一个MineGenerator生成矿
     {
         public const double A = 100;    // 金矿强度值计算参数
-        public Dot StartDot;       // 金矿初始位置
-        public Dot FinalDot;         // 需要运输到的点（停车区域的中心点）
-        public int StartDepth;           // 金矿初始深度
+        public Dot Pos;       // 金矿初始位置
+        // public Dot FinalDot;         // 需要运输到的点 // hyh认为不需要，只有第一回合随机生成一个停车点，写到MineGenerator里即可
+        public int Depth;           // 金矿初始深度
 
         //构造函数（含参）
-        public Mine(Dot start_dt, Dot end_dt, int depth_)
+        public Mine(Dot start_dt, int depth_)
         {
-            StartDot = start_dt;
-            FinalDot = end_dt;
-            StartDepth = depth_;
+            Pos = start_dt;
+            Depth = depth_;
         }
 
         // 构造函数（无参）
         public Mine()
         {
             Dot temp = new Dot(0, 0);
-            StartDot = temp;
-            FinalDot = temp;
-            StartDepth = 0;
+            Pos = temp;
+            Depth = 0;
         }
 
         //用于修改点位的接口
-        public void ResetInfo(Dot start_dt, Dot end_dt, int depth_)
+        public void ResetInfo(Dot start_dt, int depth_)
         {
-            StartDot = start_dt;
-            FinalDot = end_dt;
-            StartDepth = depth_;
+            Pos = start_dt;
+            Depth = depth_;
         }
         
         // 获取某金矿对任意点处的强度
         static public double GetIntensity(Mine m, Dot d)
         {
-            return A/(Math.Pow(m.StartDepth, 2) + Math.Pow(m.StartDot.x - d.x, 2) + Math.Pow(m.StartDot.y - d.y, 2));
+            return A/(Math.Pow(m.Depth, 2) + Math.Pow(m.Pos.x - d.x, 2) + Math.Pow(m.Pos.y - d.y, 2));
         }
     }
 }
